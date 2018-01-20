29e812d7fd6fecec1c1e5f17039dee9ee96068bd
Matan Haller <matan.haller@gmail.com>
2018-01-20 16:51:28 +0200

    Changed app structure to screen format.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""Main app file.
+"""Main app file (GUI component).
 """
 
 # Imports
@@ -6,28 +6,38 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class EntryScreen(BoxLayout):
 
-	"""Entry screen class (see .kv file for structre).
-	"""
-	
-	pass
+class EntryScreen(Screen):
+
+    """Entry screen class (see .kv file for structure).
+    """
+
+    def on_join_btn_press(self):
+        """Sends join request to server.
+        """
+
+        username = self.ids['username_input'].text
 
 
 class PypeApp(App):
 
-	"""Main app class.
-	"""
-	
-	def build(self):
-		"""App builder.
-		
-		Returns:
-		    EntryScreen: Entry screen class.
-		"""
+    """Main app class.
+    """
 
-		return EntryScreen()
+    def build(self):
+        """App builder.
+
+        Returns:
+            ScreenManager: root screen manager.
+        """
+
+        # Adding all screens to root
+        root = ScreenManager()
+        root.add_widget(EntryScreen())
+
+        return root
 
 # Running app
 if __name__ == '__main__':
