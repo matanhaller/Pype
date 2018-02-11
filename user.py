a8cfbd6afd2ba83120e1a9e05fd77e3f2230880b
Matan Haller <matan.haller@gmail.com>
2018-02-11 22:23:23 +0200

    Minor fixes.

--- user.py
+++ user.py
@@ -8,8 +8,9 @@ class User(object):
 
     Attributes:
         conn (socket.socket): Connection for communicating with user.
-        status (str): Whether the user is in call (available/occupied).
+        status (str): Whether the user is in call (available/in call).
         name (str): Username.
+        call (Call): The call in which the user is in (None if he's not).
     """
 
     def __init__(self, name, conn):
@@ -23,12 +24,13 @@ class User(object):
         self.name = name
         self.conn = conn
         self.status = 'available'
+        self.call = None
 
     def switch_status(self):
         """Switching user status.
         """
 
         if self.status == 'available':
-            self.status = 'occupied'
+            self.status = 'in call'
         else:
             self.status = 'available'
