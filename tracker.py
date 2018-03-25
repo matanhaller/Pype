b5b31d0a1be648ef027bddf0ca34a319de929beb
Matan Haller <matan.haller@gmail.com>
2018-03-25 14:39:19 +0300

    Switched to latency plot.

--- tracker.py
+++ tracker.py
@@ -43,6 +43,8 @@ class Tracker(object):
 
     Attributes:
         arrived_lst (list): Temporary list of packets that have arrived out of order.
+        call_start (float): Timestamp of user join to call.
+        first_packet_flag (bool): Flag indicating whether the arrived packet is the first one.
         last_update_dct (dict): Dictionary mapping statistics type to its last update.
         lost_packets (int): The number of lost packets.
         recvd_packets_framedrop (int): The number of received packets
@@ -51,16 +53,18 @@ class Tracker(object):
         stat_dct (dict): Dictionary mapping statistics type to value.
         tracking_dct (dict): Dictionary for tracking sequence numbers of
          packets yet to arrive.
-        (for framedrop measurement).
-        (for framerate measurement).
+        xlst (list): Time values (for latency plot).
+        ylst (list): Latency values (for latency plot).
     """
 
     def __init__(self):
         """Constructor method
         """
+
         self.call_start = time.time()
         self.xlst = []
         self.ylst = []
+        self.first_packet_flag = True
         self.seq = 0
         self.recvd_packets_framerate = 0
         self.recvd_packets_framedrop = 0
@@ -121,11 +125,17 @@ class Tracker(object):
         # Calculating latency of received packet
         new_latency = time.time() - kwargs['timestamp']
 
+        # Adding time values to plot (for testing purposes)
+        self.xlst.append(time.time() - self.call_start)
+
         # Updating average latency
         delta_t = time.time() - self.last_update_dct['latency']
         weight = exp_weight(delta_t)
         self.stat_dct['latency'] = exp_moving_avg(
             self.stat_dct['latency'], new_latency, weight)
+
+        # Adding average latency values to plot (for testing purposes)
+        self.ylst.append(self.stat_dct['latency'] * 1000)
 
         self.last_update_dct['latency'] = time.time()
 
@@ -138,49 +148,51 @@ class Tracker(object):
 
         self.recvd_packets_framedrop += 1
 
-        # Adding pending packets to tracking dictionary
-        if kwargs['seq'] > self.seq:
-            for seq in xrange(self.seq, kwargs['seq']):
-                if seq not in self.tracking_dct and seq not in self.arrived_lst:
-                    self.tracking_dct[seq] = 0
-            self.arrived_lst.append(kwargs['seq'])
+        if self.first_packet_flag:
+            self.seq = kwargs['seq'] + 1
+            self.first_packet_flag = False
+        else:
+            # Adding pending packets to tracking dictionary
+            if kwargs['seq'] > self.seq:
+                for seq in xrange(self.seq, kwargs['seq']):
+                    if seq not in self.tracking_dct and seq not in self.arrived_lst:
+                        self.tracking_dct[seq] = 0
+                self.arrived_lst.append(kwargs['seq'])
 
-        # Removing arrived packet from tracking dictionary
-        if kwargs['seq'] in self.tracking_dct:
-            del self.tracking_dct[kwargs['seq']]
-            self.arrived_lst.append(kwargs['seq'])
-            if len(self.arrived_lst) > 3:
-                self.arrived_lst.pop(0)
+            # Removing arrived packets from tracking dictionary
+            if kwargs['seq'] in self.tracking_dct:
+                del self.tracking_dct[kwargs['seq']]
+                self.arrived_lst.append(kwargs['seq'])
+                if len(self.arrived_lst) > 3:
+                    self.arrived_lst.pop(0)
 
-        # Tracking pending packets
-        lost_packet_lst = []
-        for seq in self.tracking_dct:
-            if self.tracking_dct[seq] == 2:
-                lost_packet_lst.append(seq)
-                if kwargs['seq'] > seq:
-                    self.lost_packets += 1
-                continue
-            self.tracking_dct[seq] += 1
+            # Tracking pending packets
+            lost_packet_lst = []
+            for seq in self.tracking_dct:
+                if self.tracking_dct[seq] == 2:
+                    lost_packet_lst.append(seq)
+                    if kwargs['seq'] > seq:
+                        self.lost_packets += 1
+                    continue
+                self.tracking_dct[seq] += 1
 
-        # Removing lost packets from dictionary
-        for seq in lost_packet_lst:
-            del self.tracking_dct[seq]
+            # Removing lost packets from dictionary
+            for seq in lost_packet_lst:
+                del self.tracking_dct[seq]
+                self.seq += 1
+
             self.seq += 1
 
-        self.seq += 1
+            delta_t = time.time() - self.last_update_dct['framedrop']
+            if delta_t > 0.5:
+                # Updating average framedrop
+                new_framedrop = self.lost_packets / \
+                    float(self.lost_packets + self.recvd_packets_framedrop)
+                weight = exp_weight(delta_t)
+                self.stat_dct['framedrop'] = exp_moving_avg(
+                    self.stat_dct['framedrop'], new_framedrop, weight)
 
-        delta_t = time.time() - self.last_update_dct['framedrop']
-        if delta_t > 0.5:
-            # Updating average framedrop
-            new_framedrop = self.lost_packets / \
-                float(self.lost_packets + self.recvd_packets_framedrop)
-            self.xlst.append(kwargs['timestamp'] - self.call_start)
-            weight = exp_weight(delta_t)
-            self.stat_dct['framedrop'] = exp_moving_avg(
-                self.stat_dct['framedrop'], new_framedrop, weight)
-            self.ylst.append(self.stat_dct['framedrop'] * 100)
+                self.recvd_packets_framedrop = 0
+                self.lost_packets = 0
 
-            self.recvd_packets_framedrop = 0
-            self.lost_packets = 0
-
-            self.last_update_dct['framedrop'] = time.time()
+                self.last_update_dct['framedrop'] = time.time()
