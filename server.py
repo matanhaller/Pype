a1f66e09820103936b42629558165520007fdd29
Matan Haller <matan.haller@gmail.com>
2018-03-31 21:32:47 +0300

    Spelling fix.

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ class PypeServer(object):
         """Constructor method.
         """
 
-        # Cofiguring logger
+        # Configuring logger
         logging.basicConfig(
             format='[%(asctime)s]%(levelname)s: %(message)s',
             datefmt='%d-%m-%Y %H:%M:%S')
