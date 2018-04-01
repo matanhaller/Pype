38e23501520f1a86d25a880dc65804aa48d709b4
Matan Haller <matan.haller@gmail.com>
2018-04-01 12:43:34 +0300

    Removed JPEG compression from frame read thread.

--- webcamstream.py
+++ webcamstream.py
@@ -12,9 +12,7 @@ class WebcamStream(object):
     """Live webcam stream class.
 
     Attributes:
-        cap (cv2.VideoCapture): Description
-        COMPRESSION_QUALITY (int): Value indicating the resultant quality of frame
-         after JPEG compression.
+        cap (cv2.VideoCapture): Webcam video captuer object.
         frame (Image): Current frame.
         keep_streaming (bool): Indicates whether to keep reading frames in a seperate thread.
     """
@@ -26,26 +24,19 @@ class WebcamStream(object):
         """
 
         self.cap = cv2.VideoCapture(1)
-        self.frame = cv2.imencode('.jpg', self.cap.read()[1],
-                                  [cv2.IMWRITE_JPEG_QUALITY,
-                                   WebcamStream.COMPRESSION_QUALITY])[1]
+        self.frame = self.cap.read()[1]
         self.keep_streaming = True
         self.update_loop()
 
     @new_thread('webcam_stream_thread')
     def update_loop(self):
-        """Reads frames from webcam in a seperate thread and compresses
-        them using JPEG.
+        """Reads frames from webcam in a seperate thread.
         """
 
         while self.keep_streaming:
             ret, frame = self.cap.read()
             if ret:
-                ret, encoded_frame = cv2.imencode('.jpg', frame,
-                                                  [cv2.IMWRITE_JPEG_QUALITY,
-                                                   WebcamStream.COMPRESSION_QUALITY])
-                if ret:
-                    self.frame = encoded_frame
+                self.frame = frame
 
     def read(self):
         """Retrieves current frame from webcam.
