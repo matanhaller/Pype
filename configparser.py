784bd4153d0b5d05e4f084ec74435ac59a1fce88
Matan Haller <matan.haller@gmail.com>
2018-04-06 22:00:51 +0300

    Initial commit.

---
+++ configparser.py
@@ -0,0 +1,31 @@
+"""Configuration file parsing interface.
+
+Attributes:
+    config (ConfigParser): Configuration file parser object. 
+    CONFIG_FILE_PATH (str): Configuration file path.
+"""
+
+# Imports
+from ConfigParser import ConfigParser
+
+# Retreiving configuration options
+CONFIG_FILE_PATH = 'config.cfg'
+config = ConfigParser()
+config.readfp(open(CONFIG_FILE_PATH))
+
+
+def get_option(option):
+    """Retreives specified option from configuration file.
+
+    Args:
+        option (str): Name of option to retreive from configuration file.
+
+    Returns:
+        str/int/list: The value of the requested option.
+    """
+
+    global config
+    try:
+        return config.getint('Header', option)
+    except ValueError:
+        return config.get('Header', option)
